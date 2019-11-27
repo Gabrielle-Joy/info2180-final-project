b3e48a93f422fb8b6f47a4afbaa18184fb60552b
Gabrielle-Joy <gabzi.contact@gmail.com>
2019-11-27 21:21:29 +0000

    added two tables to sql

---
+++ schema.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.11
+--
+-- to install this database, from a terminal, type:
+-- mysql -u USERNAME -p -h SERVERNAME world < world.sql
+--
+-- Host: localhost    Database: world
+-- ------------------------------------------------------
+-- Server version   5.0.45-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP DATABASE IF EXISTS admindb;
+CREATE DATABASE admindb;
+USE admindb;
+
+--
+-- Table structure for table `cities`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL auto_increment,
+  `firstname` char(35) NOT NULL default '',
+  `lastname` char(35) NOT NULL default '',
+  `password` char(50) NOT NULL default '',
+  `email` char(50) NOT NULL default '',
+  `date_joined` date NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4080 DEFAULT CHARSET=utf8mb4;
+
+
+
+
+DROP TABLE IF EXISTS `issues`;
+CREATE TABLE `issues` (
+  `id` int(11) NOT NULL auto_increment,
+  `title` char(60) NOT NULL default '',
+  `description` varchar(100) NOT NULL default '',
+  `type` char(20) NOT NULL default '',
+  `priority` char(20) NOT NULL default '',
+  `status` char(20) NOT NULL default '',
+  `assigned_to` int(11) NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `created` date NOT NULL default '0',
+  `updated` date NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4080 DEFAULT CHARSET=utf8mb4;
