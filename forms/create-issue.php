d9d7f989fcb391f68960d3973796c42fc4a244d9
Rowan Atkinson <rowaneatkinson@gmail.com>
2019-11-29 15:27:04 -0500

    added issue form

--- forms/create-issue.php
+++ forms/create-issue.php
@@ -1,32 +1,38 @@
 <?php
-require("../scripts/htmlBuilder.php")
+require("../scripts/htmlBuilder.php");
 
-$types = []
+$typeList = ["bug" => "Bug", "proposal" => "Proposal", "task" => "Task"];
+$types = options($typeList);
+
+$pList = ["minor" => "Minor", "major" => "Major", "critical" => "Critical"];
+$priorities = options($pList);
+
+$action = "../forms/create-issue.php";
 
 echo <<< EOF
 <h1>Create Issue</h1>
 
-<form action="" onsubmit="return validate-user()">
+<form action="{$action}" onsubmit="return validate-user()" method="post">
     <label for="title">Title</label>
     <input type="text" id="title" name="title">
-
+    <br>
     <label for="description">Description</label>
     <input type="text" id="description" name="description">
-
+    <br>
     <label for="assTo">Assigned To</label>
-    <select id="assTo" name="assigned_to" size="4">
-    {users}
+    <select id="assTo" name="assigned_to"><select>
+    <br>
 
     <label for="type">Type</label>
-    <select id="type" name="type" size="4">
-    <option value="bug">Bug</option>
-    <option value="bug">Bug</option>
-    <option value="bug">Bug</option>
-
+    <select id="type" name="type">
+    {$types}
+    </select>
+    <br>
     <label for="priority">Priority</label>
-    <select id="priority" name="priority" size="4">
-    {priority}
-
+    <select id="priority" name="priority">
+    {$priorities}
+    </select>
+    <br>
     <input type="submit" value="Submit">
 </form>
 
--- scripts/htmlBuilder.php
+++ scripts/htmlBuilder.php
@@ -25,6 +25,18 @@ function th($heading) {
     return "<th>{$heading}</th>";
 }
 
+function options($optionList) {
+    $options = "<option value selected disabled hidden>--select option--</option>";
+    foreach ($optionList as $value => $display) {
+        $options .= option($value, $display);
+    }
+    return $options;
+}
+
+function option($value, $display) {
+    return "<option value={$value}>{$display}</option>";
+}
+
 function alertError( $msg ) {
     echo "<script>alert({$msg})</alert>";
 }
